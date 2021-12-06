c695370a6525091c48111de6edb4208ab1e92178
ndtr2000 <53716352+ndtr2000@users.noreply.github.com>
2021-12-06 21:47:25 +0700

    add process file

--- webapp/js/account.js
+++ webapp/js/account.js
@@ -1,3 +1,33 @@
 const $updateAccBtn = document.getElementById("updateAccBtn");
 const $deleteAccBtn = document.getElementById("deleteAccBtn");
 const $discardBtn = document.getElementById("discardBtn");
+
+
+const $usernameText = document.getElementById("username");
+const $pswText = document.getElementById("psw");
+const $repeatPswText = document.getElementById("psw-repeat");
+
+const handleUpdateAccount = async() => {
+    let psw = $pswText.value;
+    let repeatPsw = $repeatPswText.value;
+
+    let username = $usernameText.value;
+
+    if (psw == "" || username == "") {
+        alert("All fields are required");
+    } else {
+        if (psw === repeatPsw) {
+            const result = await apis.app_user.update({ user: { username: username, password: psw } })
+        } else {
+            alert("Password doesn't match");
+        }
+    }
+
+}
+
+const handleDeleteAccount = async() => {
+    const result = await apis.app_user.delete();
+}
+
+$deleteAccBtn.onclick = handleDeleteAccount;
+$updateAccBtn.onclick = handleUpdateAccount;
--- webapp/js/login.js
+++ webapp/js/login.js
@@ -1,2 +1,18 @@
-const $usernameText = document.getElementById(username);
-const $pswText = document.getElementById(psw);
+import { apis } from "../apis";
+
+const $usernameText = document.getElementById("username");
+const $pswText = document.getElementById("psw");
+const $signInBtn = document.getElementById("signInBtn");
+
+const handleSignIn = async() => {
+    username = $usernameText.value;
+    psw = $pswText.value;
+
+    if (username == "" || psw == "") {
+        alert("All fields are required");
+    } else {
+        const result = await apis.app_user.login({ user: { username: username, password: psw } })
+    }
+}
+
+$signInBtn.onclick = handleSignIn;
